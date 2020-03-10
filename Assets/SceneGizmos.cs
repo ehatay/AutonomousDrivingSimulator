6ec0636b143e7337e3822be518b9927aba1bfbc2
unknown <e.hatay@vikes.csuohio.edu>
2020-03-10 17:43:45 -0400

    Initial Commit

---
+++ Assets/SceneGizmos.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneGizmos : MonoBehaviour
+{
+    public List<Transform> car_targets;
+    public List<Transform> sign_targets;
+
+    void OnDrawGizmosSelected()
+    {
+        foreach (Transform target in car_targets)
+        {
+            if (target != null)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawLine(transform.position, target.position);
+            }
+        }
+        foreach (Transform target in sign_targets)
+        {
+            if (target != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(transform.position, target.position);
+            }
+        }
+    }
+}
